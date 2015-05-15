db5ee89437e0c55e7d8fd283170042ddd0a8b903
Christopher Fabian <christopher-fabian@web.de>
2015-05-15 11:49:54 +0200

    initial commit

---
+++ Ligaverwaltung/menu.cpp
@@ -0,0 +1,13 @@
+
+#include "stdafx.h"
+#include <iostream>
+
+void mainMenu()
+{
+	
+	std::cout << "[1] Turnierverwaltung " << std::endl;
+	std::cout << "[2] Optionen" << std::endl;
+	std::cout << "[0] Beenden" << std::endl;
+	
+}
+
