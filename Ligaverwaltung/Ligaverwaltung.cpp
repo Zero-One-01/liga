5ccf00950ec3302adb931ecb977c8e2a18dd7595
Padrio <thepadrio@gmail.com>
2015-05-19 20:53:39 +0200

    Some cosmetic changes console does now have a title a litle logo will be written

--- Ligaverwaltung/Ligaverwaltung.cpp
+++ Ligaverwaltung/Ligaverwaltung.cpp
@@ -6,13 +6,32 @@
 #include <conio.h>
 #include <ctime>
 #include "menu.h"
+#include <windows.h>
+
+void writeTitle();
 
 // ### Haupt-Einstiegspunkt
 int main()
 {
+	// ### Setze Fenster-Titel
+	SetConsoleTitle( TEXT("Ligaverwaltung v0.1") );
+
+	// ### Schreibe ein kleines Logo
+	writeTitle();
+
 	// ### Rufe das Hauptmenü auf
 	mainMenu();
 
+	
 	// ### Beende ohne Fehler-Code
 	return 0;
+}
+
+void writeTitle()
+{
+	std::cout << "###########################################" << std::endl;
+	std::cout << "#           Ligaverwaltung v0.1           #" << std::endl;
+	std::cout << "#     By: Christopher, Juric & Pascal     #" << std::endl;
+	std::cout << "###########################################" << std::endl;
+	std::cout << std::endl << std::endl;
 }
