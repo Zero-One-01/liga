145c9cd44a59aabb4bb77681df2b0ea77ffcb21a
Padrio <thepadrio@gmail.com>
2015-05-19 20:52:31 +0200

    Added writeInstruction(); method

--- Ligaverwaltung/menu.cpp
+++ Ligaverwaltung/menu.cpp
@@ -3,9 +3,18 @@
 #include <iostream>
 #include <conio.h>
 
+
+void writeInstruction()
+{
+	std::cout << "Bitte wählen Sie eine Aktion aus: ";
+}
+
 void mainMenu()
 {
 	
+	// ### Vor jedem Schreiben eines Menu's eine Anweisung ausgeben
+	writeInstruction();
+
 	std::cout << "[1] Turnierverwaltung " << std::endl;
 	std::cout << "[2] Optionen" << std::endl;
 	std::cout << "[0] Beenden" << std::endl;
@@ -14,16 +23,19 @@ void mainMenu()
 
 	do
 	{
-		input = _getch();
+		std::cin >> input;
 
 		switch (input)
 		{
-		case '1':
-			break;
-		case '2':
-			break;
-		default:
-			break;
+			case '1':
+				// ### Anweisung 1
+				break;
+			case '2':
+				// ### Anweisung 2
+				break;
+			default:
+				// ### Breche Software ab
+				break;
 		}
 
 	} while (input != '0');
